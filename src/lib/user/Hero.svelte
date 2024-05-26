e486bed1cbed39c062dc48566652eee8c69ffb84
Austin Caron <austin.caron1@gmail.com>
2024-05-26 16:48:26 -0400

    adding skeleton of user hero

---
+++ src/lib/user/Hero.svelte
@@ -0,0 +1,44 @@
+<script>
+  let ranks = [
+    {
+      rank: 'Diamond',
+      division: 'IV'
+    },
+    {
+      rank: 'Emerald',
+      division: 'IV'
+    },
+    {
+      rank: 'Gold',
+      division: 'III'
+    },
+  ]
+</script>
+
+<div class="hero__profile">
+  <img src="/" class="hero__icon" alt="">
+  <div class="hero__user">
+    <div class="hero__ranks">
+      {#each ranks as {rank, division}}
+        <div class="hero__rank">
+          <span class="hero__rank-name">{rank} {division}</span>
+        </div>
+      {/each}
+    </div>
+    <h1 class="hero__name">Threekings236 <span class="hero__tagline">#NA1</span></h1>
+    <button class="hero__update">Update</button>
+  </div>
+</div>
+<div class="hero__match">
+  <h3 class="hero__subtitle">Most Recent Match:</h3>
+  <div class="hero__placement">
+    <img src="/" alt="" class="hero__tactician">
+    <h3 class="hero__subtitle hero__subtitle--placement">4th Place</h3>
+    <div class="hero__traits hero__traits--match">
+      <img src="/" class="hero__trait hero__trait--match" alt="">
+    </div>
+  </div>
+  <div class="hero__units">
+    <img src="/" class="hero__unit hero__unit--match" alt="">
+  </div>
+</div>
---
+++ src/routes/user/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Hero from '$lib/user/Hero.svelte';
+  // export let data;
+</script>
+
+<section>
+  <Hero />
+</section>
